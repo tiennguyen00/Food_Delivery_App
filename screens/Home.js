1ca7b1739dd1603ceaab5d014670e9b433bf2570
18521487@gm.uit.edu.vn <18521487@gm.uit.edu.vn>
2021-07-04 10:51:25 +0700

    Home(RestaurantList)

--- screens/Home.js
+++ screens/Home.js
@@ -1,3 +1,4 @@
+import { FontDisplay } from 'expo-font';
 import React from 'react';
 import {
   SafeAreaView,
@@ -344,6 +345,15 @@ export default function Home() {
     setSelectedCategory(categories);
   }
 
+  const getCAtegoriesById = (id) => {
+    let category = categories.filter(a => a.id == id)
+
+    if(category.length > 0) {
+      return category[0].name
+    }
+    return ''
+  }
+
   const renderMainCategories = () => {
     const renderItem = ({item}) => {
       return (
@@ -460,10 +470,129 @@ export default function Home() {
     )
   }
 
+  const renderRestaurantList = () => {
+    const renderItem = ({item}) => {
+      return (
+        <TouchableOpacity
+          style={{marginBottom: SIZES.padding *2}}
+          //onPress
+        >
+          {/* Image */}
+          <View
+            style={{
+              marginBottom: SIZES.padding
+            }}
+          >
+            <Image
+              source={item.photo}
+              resizeMode="cover"
+              style={{
+                width: '100%',
+                height: 200,
+                borderRadius: SIZES.radius
+              }}
+            />
+            <View
+              style={{
+                position: 'absolute',
+                bottom: 0,
+                height: 50,
+                width: SIZES.width * 0.3,
+                backgroundColor: COLORS.white,
+                borderTopRightRadius: SIZES.radius,
+                borderBottomLeftRadius: SIZES.radius,
+                alignItems: 'center',
+                justifyContent: 'center',
+                ...styles.shadow
+              }}
+            >
+              <Text style={{...FONTS.h4}}>{item.duration}</Text>
+            </View>
+          </View>
+
+          {/* Restauant Info */}
+          <Text style={{ ...FONTS.body2}} >{item.name}</Text>
+
+          <View
+            style={{
+              marginTop: SIZES.padding,
+              flexDirection: 'row'
+            }}
+          >
+            {/* Rating */}
+            <Image
+              source={icons.star}
+              style={{
+                height: 20,
+                width: 20,
+                tintColor: COLORS.primary,
+                marginRight: 10
+              }}
+            />
+              <Text style={{...FONTS.body3}}>{item.rating}</Text>
+              {/* Categories */}
+              <View
+                style={{
+                  flexDirection: 'row',
+                  marginLeft: 20
+                }}
+              >
+                {
+                  item.categories.map((categoriesId) => {
+                    return (
+                      <View
+                        style={{
+                          flexDirection: 'row'
+                        }}
+                        key={categoriesId}
+                      > 
+                        <Text style={{...FONTS.body3}}>{getCAtegoriesById(categoriesId)}</Text>
+                        <Text style={{...FONTS.h3, color: COLORS.darkgray}}>.</Text>
+                      </View>
+
+                    )
+                  })
+                }
+                {/* Price */}
+                {
+                  [1, 2, 3].map(priceRating => {
+                    return (
+                      <Text
+                        key={priceRating}
+                        style={{
+                          ...FONTS.body3,
+                          color: (priceRating <= item.priceRating) ? COLORS.black : COLORS.darkgray
+                        }}
+                      >
+                        $
+                      </Text>
+                    )
+                  })
+                }
+              </View>
+          </View>
+        </TouchableOpacity>
+      )
+    }
+
+    return (
+      <FlatList
+        data={restaurants}
+        keyExtractor={item => `${item.id}`}
+        renderItem={renderItem}
+        contentContainerStyle={{
+          paddingHorizontal: SIZES.padding *2,
+          paddingBottom: 30
+        }}
+      />
+    )
+  }
+
   return (
     <SafeAreaView style={styles.container, styles.screen}>
       {renderHeader()}
       {renderMainCategories()}
+      {renderRestaurantList()}
     </SafeAreaView>
   )
 }
