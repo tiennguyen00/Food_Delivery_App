10ebfa70bb59a0df5537662ceb1d827a38061ca1
18521487@gm.uit.edu.vn <18521487@gm.uit.edu.vn>
2021-07-08 16:16:23 +0700

    Restaurant(renderFoodInfo (only UI))

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -6,7 +6,8 @@ import {
   SafeAreaView, //Only for ios
   TouchableOpacity,
   Image,
-  Animated
+  Animated,
+  ColorPropType
 } from 'react-native';
 import { icons, COLORS, SIZES, FONTS } from '../constants';
 
@@ -85,10 +86,127 @@ export default function Restaurrant({ route, navigation }) {
       </View>
     )
   }
+  
+  const renderFoodInfo = () => {
+    return (
+      <Animated.ScrollView
+        horizontal
+        pagingEnabled
+        scrollEventThrottle={16}
+        snapToAlignment="center"
+        showsHorizontalScrollIndicator={false}
+        //onScroll
+      >
+        {
+          restaurant?.menu.map((item, index) => {
+            return (
+              <View
+                key={`menu-${index}`}
+                style={{ alignItems: 'center' }}
+              >
+                <View style={{ height: SIZES.height *0.35 }}>
+                  {/* food image */}
+                  <Image
+                    source={item.photo}
+                    resizeMode="cover"
+                    style={{
+                      height: "100%",
+                      width: SIZES.width
+                    }}
+                  />
+
+                  {/* Quantity */}
+                  <View
+                    style={{
+                      position: 'absolute',
+                      bottom: -20,
+                      width: SIZES.width,
+                      height: 50,
+                      justifyContent: 'center',
+                      flexDirection: 'row'
+                    }}
+                  >
+                    <TouchableOpacity
+                      style={{
+                        width: 50,
+                        backgroundColor: COLORS.white,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        borderTopLeftRadius: 25,
+                        borderBottomLeftRadius: 25
+                      }}
+                    >
+                      <Text style={{...FONTS.body1}}>-</Text>
+                    </TouchableOpacity>
+                    <View
+                      style={{
+                        width: 50,
+                        backgroundColor: COLORS.white,
+                        alignItems:'center',
+                        justifyContent: 'center'
+                      }}
+                    >
+                      <Text style={{...FONTS.h2}}>5</Text>
+                    </View>
+
+                    <TouchableOpacity
+                      style={{
+                        width:50,
+                        backgroundColor: COLORS.white,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        borderTopRightRadius: 25,
+                        borderBottomRightRadius: 25
+                      }}
+                    >
+                      <Text style={{...FONTS.body1}}>+</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+
+                {/* Name and Description */}
+                <View
+                  style={{
+                    width: SIZES.width,
+                    alignItems: 'center',
+                    marginTop: 15,
+                    paddingHorizontal: SIZES.padding *2
+                  }}
+                >
+                  <Text style={{marginVertical: 10, textAlign: 'center', ...FONTS.h2}}>{item.name} - {item.price.toFixed(2)} </Text>
+                  <Text style={{...FONTS.body3, textAlign:'center'}} >{item.description}</Text>
+
+                  {/* Categories */}
+                  <View
+                    style={{
+                      flexDirection:'row',
+                      marginTop: 10
+                    }}
+                  >
+                    <Image
+                      source={icons.fire}
+                      style={{
+                        width: 20,
+                        height: 20,
+                        marginRight: 10
+                      }}
+                    />
+
+                    <Text style={{...FONTS.body3, color: COLORS.darkgray}} >{item.calories.toFixed(2)} calo</Text>
+                  </View>
+                </View>
+              </View>
+            )
+          })
+        }
+      </Animated.ScrollView>
+    )
+  }
 
   return (
     <SafeAreaView style={styles.container, styles.screen}>
       {renderHeader()}
+      {renderFoodInfo()}
     </SafeAreaView>
   )
 }
